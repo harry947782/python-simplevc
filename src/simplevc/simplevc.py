3fc30ea1aaa573fb41b0f50f5e3518a59b3c59b9
harry <harry@alt-control.net>
2016-01-01 17:33:48 +0000

    Adding More tests

--- src/simplevc/simplevc.py
+++ src/simplevc/simplevc.py
@@ -6,6 +6,7 @@ Created on 30 Aug 2014
 
 from PIL import Image
 import random, operator
+from math import ceil, floor
 
 # the second value is the ALPHA, setting it to transparent for blank pixels allows the images to be overlayed in software
 BLANKPIXEL = 0xFF, 0x00
@@ -14,8 +15,16 @@ FILLEDPIXEL = 0x00, 0xFF
 def mult(tupl, scalar):
     return tuple(map(lambda x: scalar * x, tupl))
 
+def tuple_mult(a,b):
+    assert len(a) == len(b)
+    return tuple(map(operator.mul, a, b))
+
 def add(tupl, scalar):
     return tuple(map(lambda x: x + scalar, tupl))
+
+def tuple_add(a, b):
+    assert len(a) == len(b)
+    return tuple(map(operator.add, a, b))
 
 def allPositions(size):
     maxX, maxY = size
@@ -23,41 +32,42 @@ def allPositions(size):
         for y in range(0, maxY):
             yield (x, y)
 
-def addtuple(a, b):
-    return tuple(map(operator.add, a, b))
 
 def allZeroCentrePositions(size):
-    halfX = int(size[0] / 2)
-    halfY = int(size[1] / 2)
-    for x in range(-1*halfX, halfX):
-        for y in range(-1*halfY, halfY):
+    halfX = size[0] / 2.0
+    halfY = size[1] / 2.0
+    for x in range(-1*floor(halfX), ceil(halfX)):
+        for y in range(-1*floor(halfY), ceil(halfY)):
             yield (x, y)
 
 def padPositions(padCentrePoint, padSize):
-    for offset in allZeroCentrePositions((padSize, padSize)):
-        yield addtuple(padCentrePoint, offset)
+    for offset in allZeroCentrePositions(padSize):
+        yield tuple_add(padCentrePoint, offset)
+
+def padCentre(sourcePosition, padSize):
+    return tuple(map(floor,tuple_add(tuple_mult(sourcePosition, padSize), mult(padSize, 0.5))))
 
 def randomHalfOf(vals):
     return random.sample(vals, int(len(vals) / 2))
 
-def generateKeySetPositions(size, padsize):
-    for position in allPositions(size):
-        getPadCentre = add(mult(position, padsize), int(padsize / 2))
-        allPadPositions = list(padPositions(getPadCentre, padsize))
+def generateKeySetPositions(sourceSize, padSize):
+    for sourcePosition in allPositions(sourceSize):
+        pad = padCentre(sourcePosition, padSize)
+        allPadPositions = list(padPositions(pad, padSize))
         for padPos in randomHalfOf(allPadPositions):
             yield padPos
 
-def createKeyImage(size, padsize):
-    keyIm = Image.new('LA', mult(size, padsize), BLANKPIXEL)
-    for pos in generateKeySetPositions(size, padsize):
+def createKeyImage(sourceSize, padsize):
+    keyIm = Image.new('LA', tuple_mult(sourceSize, padsize), BLANKPIXEL)
+    for pos in generateKeySetPositions(sourceSize, padsize):
         keyIm.putpixel(pos, FILLEDPIXEL)
     return keyIm
 
 def isPixelSet(pixelValue):
     return pixelValue > 0x00
 
-def invertPixel(cipherIm, pos):
-    cipherIm.putpixel(pos, add(mult(cipherIm.getpixel(pos), -1), 0xFF))
+def invertPixel(im, pos):
+    im.putpixel(pos, add(mult(im.getpixel(pos), -1), 0xFF))
 
 def invertPad(cipherIm, padCentre, padSize):
     for padPos in padPositions(padCentre, padSize):
@@ -68,24 +78,21 @@ def getSetPixels(image):
         if isPixelSet(image.getpixel(position)):
             yield position
 
-def getPadCentre(position, padSize):
-    return add(mult(position, padSize), int(padSize / 2))
-
 def createCipherImage(keyIm, plainIm, padSize):
-    assert keyIm.size == mult(plainIm.size, padSize)
+    assert keyIm.size == tuple_mult(plainIm.size, padSize)
     assert plainIm.mode == 'L'
     assert keyIm.mode == 'LA'
     cipherIm = keyIm.copy()
     for position in getSetPixels(plainIm):
-        padCentre = getPadCentre(position, padSize)
-        invertPad(cipherIm, padCentre, padSize)
+        pad = padCentre(position, padSize)
+        invertPad(cipherIm, pad, padSize)
     return cipherIm
 
 
 if __name__ == '__main__':
     plainIm = Image.open("cubs.ppm").convert(mode='L')
-    keyIm = createKeyImage(plainIm.size, 2)
+    keyIm = createKeyImage(plainIm.size, (2,2))
     keyIm.save("key.png", "PNG")
-    cipherIm = createCipherImage(keyIm, plainIm, 2)
+    cipherIm = createCipherImage(keyIm, plainIm, (2,2))
     cipherIm.save("right.png", "PNG")
 
--- test.py
+++ test.py
@@ -1,9 +1,59 @@
+from simplevc.simplevc import mult, add, allPositions, tuple_add, tuple_mult,\
+    allZeroCentrePositions, padPositions, randomHalfOf, padCentre
 import unittest
 
-class TestStringMethods(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+class TestStringMethods(unittest.TestCase):
+    def test_mult(self):
+        self.assertEqual((2,4,6,8), mult((1,2,3,4),2))
+        
+    def test_tuple_mult(self):
+        self.assertEqual((3,8), tuple_mult((1,2),(3,4)))
+    
+    def test_add(self):
+        self.assertEqual((3,4,5,6), add((1,2,3,4),2))
+    
+    def test_allPositions_empty(self):
+        self.assertEqual([], list(allPositions((0,0))))
+        
+    def test_allPositions_squares(self):
+        self.assertEqual([(0,0)], list(allPositions((1,1))))
+        self.assertEqual([(0,0),(0,1),(1,0),(1,1)], list(allPositions((2,2))))
+        self.assertEqual([(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)], list(allPositions((3,3))))
+    
+    def test_allPositions_rects(self):
+        self.assertEqual([(0,0),(0,1)], list(allPositions((1,2))))
+        self.assertEqual([(0,0),(1,0)], list(allPositions((2,1))))
+    
+    def test_tuple_add(self):
+        self.assertEqual((4,6), tuple_add((1,2),(3,4)))
+        
+    def test_allZeroCentrePositions_square(self):
+        self.assertEqual([(0,0)],list(allZeroCentrePositions((1,1))))
+        self.assertEqual([(-1,-1),(-1,0),(0,-1),(0,0)],list(allZeroCentrePositions((2,2))))
+        self.assertEqual([(-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)],list(allZeroCentrePositions((3,3))))
+        
+    def test_allZeroCentrePositions_rect(self):
+        self.assertEqual([(0,-1),(0,0)],list(allZeroCentrePositions((1,2))))
+        self.assertEqual([(-1,0),(0,0)],list(allZeroCentrePositions((2,1))))
+    
+    def test_padPositions(self):
+        self.assertEqual([(4,7),(4,8),(5,7),(5,8)],list(padPositions((5,8),(2,2))))
+        self.assertEqual([(4,7),(4,8),(4,9),(5,7),(5,8),(5,9),(6,7),(6,8),(6,9)],list(padPositions((5,8),(3,3))))
+        
+    def test_padCentre(self):
+        self.assertEqual((0,0),padCentre((0,0),(1,1)))
+        self.assertEqual((1,1),padCentre((0,0),(2,2)))
+        self.assertEqual((1,1),padCentre((0,0),(3,3)))
+        self.assertEqual((2,2),padCentre((0,0),(4,4)))
+        self.assertEqual((1,1),padCentre((1,1),(1,1)))
+        self.assertEqual((3,3),padCentre((1,1),(2,2)))
+        self.assertEqual((4,4),padCentre((1,1),(3,3)))
+        self.assertEqual((6,6),padCentre((1,1),(4,4)))
+    
+    def test_randomHalfOf(self):
+        self.assertEqual(5, len(randomHalfOf([0,1,2,3,4,5,6,7,8,9])))
+        self.assertEqual(4, len(randomHalfOf([0,1,2,3,4,5,6,7,8])))
 
 if __name__ == '__main__':
     unittest.main()
